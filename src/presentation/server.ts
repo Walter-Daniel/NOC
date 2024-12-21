037b74d7e335671ba6595f099ea4c3962f32abd6
Walter <walterdcarrizo.19@gmail.com>
2024-12-21 10:02:44 -0300

    add cron job functionality with CronService and update dependencies

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,12 @@
+import { CronService } from './cron/cron-service';
+
 export class Server {
   public static start() {
     console.log('Server started');
+
+    CronService.createJob('*/5 * * * * *', () => {
+      const date = new Date();
+      console.log('Cron job running', date);
+    });
   }
 }
